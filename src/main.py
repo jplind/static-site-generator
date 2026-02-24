65d279084a7a36d51b74e24c12ad07793ea5177a
jplind <dullahan10@hotmail.com>
2026-02-24 17:51:38 +0100

    fix deployment pathing

--- src/main.py
+++ src/main.py
@@ -2,11 +2,17 @@ from generate_docs import generate_docs
 from generate_pages_recursive import generate_pages_recursive
 import sys
 
+dir_path_static = "./static"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
+default_base_path = "/"
+
 def main():
 	generate_docs()
-	base_path = "/"
+	base_path = default_base_path
 	if len(sys.argv) > 1:
 		base_path = sys.argv[1]
-	generate_pages_recursive(base_path, "content", "template.html", "docs")
+	generate_pages_recursive(base_path, dir_path_content, template_path, dir_path_public)
 
 main()
